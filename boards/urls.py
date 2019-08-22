8ee26d753d390a98dca83f8cbd6fdaa9feacfd6f
zhang <297266081@qq.com>
2019-08-22 17:03:11 +0800

    ‘表单处理

--- boards/urls.py
+++ boards/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'boards'
 urlpatterns = [
     url(r'(?P<pk>\d+)/$',views.board_topics,name='board_topics'),
+    url(r'(?P<pk>\d+)/new/$',views.new_topic,name='new_topic'),
 ]
 
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,get_object_or_404
-
-# Create your views here.
-from .models import Board
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Board,Topic,Post
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -11,3 +11,25 @@ def home(request):
 def board_topics(request,pk):
     board = get_object_or_404(Board,pk=pk)
     return render(request,'topics.html',{'board':board})
+
+
+def new_topic(request,pk):
+    board = get_object_or_404(Board,pk=pk)
+    user = User.objects.first()
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                    message=form.cleaned_data.get('message'),
+                    topic=topic,
+                    created_by=user,
+                )
+            return redirect('boards:board_topics',pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request,'new_topic.html',{'board':board,'form':form})
